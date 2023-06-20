11974b80b0e6fbd3df6ef023d4f174021e97b646
Yashvardhan Arora <yash22arora@gmail.com>
2023-06-20 22:34:01 +0530

    Setup Axios client instance and install react-query
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ConfigProvider } from "antd";
 import Form from "./pages/Form/Form";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
